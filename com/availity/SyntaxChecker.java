1058c0294a3b1c46c67a1ff1e942cfd368d788e9
Chris Havekost <kostylee23@gmail.com>
2019-04-06 15:41:38 -0400

    Add SyntaxChecker for validating LISP code.

---
+++ com/availity/SyntaxChecker.java
@@ -0,0 +1,13 @@
+package com.availity;
+
+import java.util.Stack;
+
+public class SyntaxChecker {
+  /**
+   * Class that validates parentheses of LISP code.
+   * Takes a string as input and returns true if all parentheses are properly closed and nested.
+   */
+    public static void main(String[] args) throws IOException {
+
+    }
+}
