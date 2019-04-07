bdc4f7be72576ade723810a777d01ae976063b19
Chris Havekost <kostylee23@gmail.com>
2019-04-07 14:43:18 -0400

    Add EnrollmentHelper base.

---
+++ com/availity/EnrollmentHelper.java
@@ -0,0 +1,15 @@
+package com.availity;
+
+public class EnrollmentHelper {
+    /**
+     * Class to help parse enrollment files.
+     * Takes in CSV files that include a User Id (string), First and Last name (string), Version (integer),
+     * and Insurance Company (string).
+     * Separates enrollees by insurance company into separate files.
+     * Sorts contents of each new file by first and last name (ascending).
+     * Removes duplicate User Ids for the same insurance company, keeping highest version.
+     */
+    public static void main(String[] args) {
+
+    }
+}
